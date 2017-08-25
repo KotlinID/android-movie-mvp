d1a27692877fd8d9bfd9f567cffd52f9e9080bcf
Budi Oktaviyan Suryanto <budioktaviyans@gmail.com>
2017-08-25 12:28:53 +0700

    [Budi] Add extensions for utility

---
+++ app/src/main/java/id/kotlin/training/movies/ext/Commons.kt
@@ -0,0 +1,32 @@
+package id.kotlin.training.movies.ext
+
+import android.content.Context
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.MemoryCategory
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
+
+const val DEFAULT_DATE = "dd MMMM yyyy"
+
+internal fun getDate(date: String): String {
+    val format = DateTimeFormat.forPattern(DEFAULT_DATE)
+    return DateTime(date).toString(format)
+}
+
+internal fun loadImage(context: Context,
+                       url: String,
+                       imageView: ImageView) {
+
+    fun setMemoryCategory(context: Context) {
+        Glide.get(context).setMemoryCategory(MemoryCategory.NORMAL)
+    }
+
+    setMemoryCategory(context)
+    GlideApp.with(context)
+            .load(url)
+            .centerCrop()
+            .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
+            .into(imageView)
+}
---
+++ app/src/main/java/id/kotlin/training/movies/ext/Images.kt
@@ -0,0 +1,7 @@
+package id.kotlin.training.movies.ext
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class Images : AppGlideModule()
